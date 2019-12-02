fe7c01a1756f2237b4d5b1485a4b9487489a6543
Vala Roff <valaroff@gmail.com>
2019-12-02 09:54:29 +0000

    first class

--- NaNair/nanair.py
+++ NaNair/nanair.py
@@ -1,1 +1,13 @@
 # velkomin 
+
+class CrewUI(object):
+    ''' UI class for crew'''
+    def __init__(self):
+        pass
+    
+    def show_crew(self):
+        pass
+
+    def show_working_crew(self):
+
+
