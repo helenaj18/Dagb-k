b7574a8b4c084f80cfeb359a4975e3dd0b71bcef
Helena Jónsdóttir <helenaj18@ru.is>
2019-12-02 12:10:02 +0000

    Laga

---
+++ NaNair/LL/flightroute.py
@@ -0,0 +1,7 @@
+
+class FlightRoute:
+    def __init__(self, dep_location, dep_time, dest, duration):
+        self.__departure_location = dep_location
+        self.__departure_time = dep_time
+        self.__destination = dest
+        self.__duration = duration
