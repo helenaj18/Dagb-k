43d03e5530539296fa9ea09e0c80aba4f1a4569b
Vala Roff <valaroff@gmail.com>
2019-12-02 11:01:29 +0000

    flightroute class

--- NaNair/nanair.py
+++ NaNair/nanair.py
@@ -186,6 +186,12 @@ class FlightAttendant(Crew):
         self.head_flight_att = True
         
 
+class FlightRoute:
+    def __init__(self, dep_location, dep_time, dest, duration):
+        self.__departure_location = dep_location
+        self.__departure_time = dep_time
+        self.__destination = dest
+        self.__duration = duration
 
 
 # IO LAYER 
