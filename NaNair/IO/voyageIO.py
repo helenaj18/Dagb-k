b8d0586d822860f7c54359c21664cadd6c8070c0
Helena <helenaj18@ru.is>
2019-12-03 11:18:45 +0000

    files

--- NaNair/IO/voyageIO.py
+++ NaNair/IO/voyageIO.py
@@ -3,13 +3,9 @@ class VoyageIO:
 
     def __init__(self):
         # Muna að breyta í rétt nöfn!
-<<<<<<< HEAD
-        self.__upcomingFlights_filename = '/Users/helenajonsdottir/Desktop/Verklegt1/Verklegt/PastFlights.csv'
+        self.__upcomingFlights_filename = '/Users/helenajonsdottir/Desktop/Verklegt1/Verklegt/UpcomingFlights.csv'
         self.__pastFlights_filename = '/Users/helenajonsdottir/Desktop/Verklegt1/Verklegt/PastFlights.csv'
-=======
-        self.__upcomingFlights_filename = '/Users/erlaarnalds/Documents/GitHub/Dagbok/UPDATEDSTUDENTDATA/UpcomingFlights.csv'
-        self.__pastFlights_filename = '/Users/erlaarnalds/Documents/GitHub/Dagbok/UPDATEDSTUDENTDATA/PastFlights.csv'
->>>>>>> 06969d924bb6538a3b104e0b8cce1da53b79d2ca
+        self.loadVoyageFromFile()
 
     def get_info(self,file_object):
         a_list = []
@@ -40,16 +36,40 @@ class VoyageIO:
         '''Loads existing voyages from the file'''
         self.read_file()
         voyage_list = []
-        for i in range(len(self.flights_list)):
-            voyage_list.append(self.flights_list[i:i+2])
+        
+        for i in range(len(self.flights_list)-2):
+            voyage_list.append(self.flights_list[i]+self.flights_list[i+2])
             i += 2
 
         self.voyage_list = voyage_list
-        print(voyage_list)
 
-    def changeVoyageInFile(self):
-        '''Changes an existing voyage in the file'''
-        pass
+# miðað við að það sé slegið inn flugnúmer á leið út
+    def changeDepartureDateTimeOfVoyage(self, new_datetime_str,flight_number):
+        '''Changes the departure date and time of an existing voyage in the file'''
+        for i in range(len(self.flights_list)):
+            if flight_number == self.flights_list[i][0]:
+                self.flights_list[i][3] = new_datetime_str
+
+        self.changeVoyageFile()
+
+    
+    def changeArrivalDateTimeOfVoyage(self, new_datetime_str,flight_number):
+        '''Changes the departure date and time of an existing voyage in the file'''
+        for i in range(len(self.flights_list)):
+            if flight_number == self.flights_list[i][0]:
+                self.flights_list[i][4] = new_datetime_str
+        
+        self.changeVoyageFile()
+
+
+    def changeVoyageFile(self):
+        '''Updates the file with new changes'''
+        voyage_str = ''
+        for item in self.flights_list:
+            voyage_str += ','.join(item) + '\n'
+        
+        file_object = open(self.__upcomingFlights_filename,'w')
+        file_object.write(voyage_str)
 
 
     def addVoyageToFile(self,new_voyage_str):
@@ -60,5 +80,8 @@ class VoyageIO:
 
         return file_object
 
+
+a = VoyageIO()
+a.changeArrivalDateTimeOfVoyage('prufa','NA1111')
 
 
