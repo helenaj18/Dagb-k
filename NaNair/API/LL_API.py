628b7ce2616e8d4b244b473947703ae1eb0ec155
Vala Roff <valaroff@gmail.com>
2019-12-03 09:33:27 +0000

    LL API

--- NaNair/API/LL_API.py
+++ NaNair/API/LL_API.py
@@ -4,6 +4,7 @@ from LL.airplaneLL import AirplaneLL
 
 class LL_API:
 
+    ### AIRPLANE LL 
     def show_all_planes(self):
         print('''Fetches all airplanes and returns it''')
         
@@ -14,8 +15,15 @@ class LL_API:
         '''Fetches one specific plane and returns it'''
         return AirplaneLL().getOneAirplane(plane_ID)
     
+    def add_airplane(self):
+        ''' Sends info for new ariplane to be added'''
+        return AirplaneLL().addAirplane()
+
+    ### CREW LL 
+    
     def get_crew(self):
         ''' Fetches all crew members'''
-        pass
+
+        return AirplaneLL
 
 #LL_API.show_all_planes()
--- NaNair/main.py
+++ NaNair/main.py
@@ -3,3 +3,4 @@ from UI.airplaneUI import AirplaneUI
 a = AirplaneUI()
 a.showAllPlanes()
 a.showOnePlane()
+a.addAirplane()
