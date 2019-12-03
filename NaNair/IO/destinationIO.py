a6ed70a6e359f56a5a948d3235554c299ba56341
kingamaris <54936816+kingamaris@users.noreply.github.com>
2019-12-03 10:06:19 +0000

    destination ready

--- NaNair/IO/destinationIO.py
+++ NaNair/IO/destinationIO.py
@@ -1,46 +1,61 @@
 class DestinationIO:
 
-    FLIGHTNUMBER = 0
-    DEPARTINGFROM = 1
-    ARRAVINGAT = 2
-    DEPARTURE = 3
-    ARRIVAL = 4
-
     def __init__(self):
         # Muna að breyta í upcomingflights.csv!!!
-        self.__destination_filename = '/Users/kingamaris/Documents/GitHub/Dagbok/STUDENTDATA/UpcomingFlights.csv'
+        self.__destination_filename = '/Users/kingamaris/Documents/GitHub/Dagbok/UPDATEDSTUDENTDATA/Destinations.csv'
 
     def read_file(self):
         '''Reads file and returns employees list'''
         file_object = open(self.__destination_filename,'r')
-        all_destination_list = []
+        destination_list = []
 
         for line in file_object:
             line = line.strip().split(',')
-            all_destination_list.append(line)
+            destination_list.append(line)
         
-        self.all_destination_list = all_destination_list
-
-    def find_destination(self):
-        '''Finds all destinations in file and returns a list of them'''
-
-        destination_list = []
-        for i in range(1,len(self.all_destination_list)):
-            destination_list.append(self.all_destination_list[i][1]) #ATH FASTAN
-        
-        destination_list = set(destination_list)
         self.destination_list = destination_list
+
+    # def find_destination(self):
+    #     '''Finds all destinations in file and returns a list of them'''
+    #     destination_list = []
+    #     for i in range(1,len(self.all_destination_list)):
+    #         destination_list.append(self.all_destination_list[i]) #ATH FASTAN
+        
+    #     self.destination_list = destination_list
 
     def loadDestinationFromFile(self):
         '''Fetches destination from a file'''
         self.read_file()
-        self.find_destination()
 
         return self.destination_list
 
-    def changeDestinationFromFile(self):
-        '''Change the destination or other destination info in file'''
-        pass
+    def ChangeEmergencyContact(self,destination_name,new_emergency_contact):
+        '''Changes the Emergency Contact for destination in file'''
+        self.__destination_name = destination_name
+        for i in range(len(self.destination_list)):
+            if destination_name == self.destination_list[i][1]:
+                self.destination_list[i][-2] = new_emergency_contact
+        
+        self.changeDestinationFile()
+    
+
+    def changeDestinationFile(self):
+        a_str = ''
+        for item in self.destination_list:
+            a_str += ','.join(item) + '\n'
+
+        file_object = open(self.__destination_filename,'w')
+        file_object.write(a_str)
+
+    def ChangeEmergencyPhone(self,destination_name,new_emergency_phone):
+        '''Changes the Emergency Contact for destination in file'''
+        self.__destination_name = destination_name
+        for i in range(len(self.destination_list)):
+            if destination_name == self.destination_list[i][1]:
+                self.destination_list[i][-1] = new_emergency_phone
+        
+        self.changeDestinationFile()
+
 
     def addDestinationFromFile(self,new_destination_str):
         '''Adds the destination into file'''
@@ -52,8 +67,9 @@ class DestinationIO:
 
 
 a = DestinationIO()
-
-
 b= a.loadDestinationFromFile()
+a.ChangeEmergencyContact('Kulusuk','Kinga')
+a.ChangeEmergencyPhone('Kulusuk','5812345')
+
 print(b)
-print(a)
+#print(a)
