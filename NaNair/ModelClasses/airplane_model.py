c8f237ffc059e612127a2f5c698890d2e233ef45
Helena <helenaj18@ru.is>
2019-12-04 16:50:48 +0000

    private breytur i airplane model

--- NaNair/ModelClasses/airplane_model.py
+++ NaNair/ModelClasses/airplane_model.py
@@ -1,24 +1,27 @@
 FORMAT_LENGTH_const = 20
 
 class Airplane:
-    def __init__(self, planeInsignia, planeTypeId,manufacturer,model,capacity,emptyWeight,maxTakeoffWeight,unitThrust,serviceCeiling,length,height,wingspan):
-        self.planeInsignia = planeInsignia
-        self.planeTypeID = planeTypeId
-        self.manufacturer = manufacturer
-        self.model = model
-        self.capacity = capacity
-        self.emptyWeight = emptyWeight
-        self.maxTakeoffWeight = maxTakeoffWeight
-        self.unitThrust = unitThrust
-        self.serviceCeiling = serviceCeiling
-        self.length = length
-        self.height = height
-        self.wingspan = wingspan
+    def __init__(self, planeInsignia, planeTypeID,manufacturer,model,capacity,emptyWeight,maxTakeoffWeight,unitThrust,serviceCeiling,length,height,wingspan):
+        self.__planeInsignia = planeInsignia
+        self.__planeTypeID = planeTypeID
+        self.__manufacturer = manufacturer
+        self.__model = model
+        self.__capacity = capacity
+        self.__emptyWeight = emptyWeight
+        self.__maxTakeoffWeight = maxTakeoffWeight
+        self.__unitThrust = unitThrust
+        self.__serviceCeiling = serviceCeiling
+        self.__length = length
+        self.__height = height
+        self.__wingspan = wingspan
 
+
+    def get_planeTypeID(self):
+        return self.__planeTypeID
 
     def __str__(self):
-        format_str = '{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}'.format(self.planeInsignia,self.planeTypeID,self.manufacturer,self.model,\
-        self.capacity,self.length,self.height,self.wingspan)
+        format_str = '{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}'.format(self.__planeInsignia,self.__planeTypeID,self.__manufacturer,self.__model,\
+        self.__capacity,self.__length,self.__height,self.__wingspan)
         
         return format_str
 
