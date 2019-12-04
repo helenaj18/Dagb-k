88b2e860b3ce58ae605286e800bd530c6993eb57
Vala Roff <valaroff@gmail.com>
2019-12-04 11:29:42 +0000

    dest model

--- NaNair/LL/destinationLL.py
+++ NaNair/LL/destinationLL.py
@@ -36,11 +36,3 @@ class DestinationLL:
     # LINKA VIÐ EDIT_MENU DESTINATION
 
 
-class Destination:
-    def __init__(self,name,airport,distance,contact,emergency_phone_number,duration):
-        self.__name = name
-        self.__airport = airport
-        self.__distance = distance
-        self.__contact = contact
-        self.__emergency_phone_number = emergency_phone_number
-        self.__duration = duration
