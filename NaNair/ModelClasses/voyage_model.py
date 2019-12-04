590815a0993fbfd64c74dd506d5d8ae626e3cb68
Vala Roff <valaroff@gmail.com>
2019-12-04 12:24:35 +0000

    voyages

--- NaNair/ModelClasses/voyage_model.py
+++ NaNair/ModelClasses/voyage_model.py
@@ -1,7 +1,8 @@
 class Voyage:
-    def __init__(self, airplane, pilot, flight_att, flight_route, voyage_id):
+    def __init__(self, airplane, pilot,copilot, head_flight_att, flight_att_one,flight_att_two, flight_route, voyage_id):
         self.__airplane = airplane
         self.__pilot = pilot
+        self.__copilot = copilot
         self.__flight_att = flight_att
         self.__flight_route = flight_route
         self.__voyage_id = voyage_id
