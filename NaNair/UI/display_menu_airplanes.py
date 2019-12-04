0e948e0426a523549f5af9eb3af1bce779118d2a
Helena <helenaj18@ru.is>
2019-12-04 14:35:06 +0000

    display menu airplanes changes

--- NaNair/UI/display_menu_airplanes.py
+++ NaNair/UI/display_menu_airplanes.py
@@ -10,8 +10,7 @@ class DisplayMenuAirplanes:
         print('#'*20)
         print()
 
-        start = True
-        while start: 
+        while True: 
             print('What would you like to display?')
             print()
             print('1 - List all airplanes')
@@ -26,7 +25,6 @@ class DisplayMenuAirplanes:
                 airplanes = AirplaneUI().showAllPlanes()
                 for elem in airplanes:
                     print(elem)
-                start = False
 
             elif selection == '2':
                 print('What type would you like to list?')
@@ -34,16 +32,15 @@ class DisplayMenuAirplanes:
                 print('NAFokkerF28')
                 print('NABAE146')
                 print()
-                
+
                 planeTypeID = input()
                 airplanes = AirplaneUI().showAirplanesByType(planeTypeID)
                 for elem in airplanes:
                     print(elem)
 
-                start = False
 
             elif selection == 'm':
                 # fara aftur á display
-                start = False
+                return
             else: 
                 print('Invalid selection')
