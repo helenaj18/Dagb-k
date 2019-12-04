abb352ae99eb17af630d5b1acce88ce80ce8a812
Helena <helenaj18@ru.is>
2019-12-04 09:28:54 +0000

    destination LL og IO

--- NaNair/LL/destinationLL.py
+++ NaNair/LL/destinationLL.py
@@ -1,18 +1,40 @@
 from API.IO_API import IO_API
+from IO.destinationIO import DestinationIO
+
+DESTINATION_NAME_const = 1
+EMERGENCY_CONTACT_NAME_const = 5
+EMERGENCY_CONTACT_PHONE_const = 6
 
 class DestinationLL:
     def __init__(self):
-        pass
+        self.__destination_list = IO_API().loadDestinationFromFile()
  
-    def getDestination(self):
-        ''' Gets destination from Destination class'''
-        destinations_set = IO_API().loadDestinationFromFile()
-        
-        return destinations_set
 
-    def changeDestination(self,destination_name, new_emergency_phone):
-        pass 
-    # ÞARF AÐ VERA ANNAÐ HVORT NAME EÐA PHONE 
+    def getDestination(self): #skoða, þarf þetta að vera sér fall?
+        ''' Gets destination from Destination class'''
+
+        return self.__destination_list
+
+
+    def changeEmergencyContactName(self,destination_name,new_emergency_contact):
+        '''Changes the Emergency Contact name for destination in file'''
+
+        for i in range(len(self.__destination_list)):
+            if destination_name == self.__destination_list[i][DESTINATION_NAME_const]:
+                self.__destination_list[i][EMERGENCY_CONTACT_NAME_const] = new_emergency_contact
+        
+        DestinationIO().changeDestinationFile(self.__destination_list)
+    
+
+    def changeEmergencyContactPhone(self,destination_name,new_emergency_phone):
+        '''Changes the Emergency Contact Phone number for destination in file'''
+        
+        for i in range(len(self.__destination_list)):
+            if destination_name == self.__destination_list[i][DESTINATION_NAME_const]:
+                self.__destination_list[i][EMERGENCY_CONTACT_PHONE_const] = new_emergency_phone
+        
+        DestinationIO().changeDestinationFile(self.__destination_list)
+
     # LINKA VIÐ EDIT_MENU DESTINATION
 
 
