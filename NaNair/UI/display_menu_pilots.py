6e71d1f6956b708fe4af3af74ee667a658d16543
Marinella <marinella16@ru.is>
2019-12-04 10:19:28 +0000

    menus

--- NaNair/UI/display_menu_pilots.py
+++ NaNair/UI/display_menu_pilots.py
@@ -1,29 +1,28 @@
-from UI.airplaneUI import AirplaneUI
+from UI.crewUI import CrewUI
 
-class DisplayMenuAirplanes: 
+class DisplayMenuPilots: 
     def __init__(self, logic_layer):
-        print('Display airplanes')
+        print('Display pilots')
 
-    def startDisplayAirplanes(self):
-        print('DISPLAY - Airplanes')
+    def startDisplayPilots(self):
+        print('DISPLAY - Pilots')
 
         start = True
         while start: 
             print('What would you like to display?')
-            print('1 - List all airplanes')
-            print('2 - List airplanes by type')
+            print('1 - Pilots with a license for a specific airplane')
+            print('2 - All pilots sorted by license')
             print('m - Go back to display menu')
 
             selection = input()
 
             if selection == '1':
-                #lista upp allar flugvelar 
-                AirplaneUI().ShowAllPlanes()
+                #lista upp flugmenn með x leyfi
                 start = False
 
 
             elif selection == '2':
-                # lista eftir typu
+                # lista upp alla flugmenn eftir hvaða leyfi þeir hafa
                 start = False
 
             elif selection == 'm':
