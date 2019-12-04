aec7f2e70f5b22920d923d33d1f791e008b2df64
Erla Arnalds <erlaa18@ru.is>
2019-12-04 14:07:50 +0000

    klasaerfðir lagaðar

--- NaNair/ModelClasses/pilot_model.py
+++ NaNair/ModelClasses/pilot_model.py
@@ -2,7 +2,7 @@ from ModelClasses.crew_model import Crew
 
 class Pilot(Crew):
     def __init__(self, name, crewID, address='', phonenumber='', email='', pilot_license='', captain=False):
-        Crew.__init__(name, crewID, address, phonenumber, email)
+        Crew.__init__(self, name, crewID, address, phonenumber, email)
         self.pilot_license = pilot_license
         self.captain = captain
  
