dcc1e75d053298ea9f02b8943512c2a728dfe1dd
Helena <helenaj18@ru.is>
2019-12-05 15:40:21 +0000

    not working crew á dates sem eru i file

--- NaNair/UI/display_menu_employee.py
+++ NaNair/UI/display_menu_employee.py
@@ -28,13 +28,12 @@ class DisplayMenuEmployee:
             print('4 - Flight attendants')
             print('5 - Working status by date')
             print('6 - Work Schedule for employee by ID')
-            print('m - back to display menu')
             print()
             selection = input()
             
 
             if selection == '1':
-                CrewUI().showCrew()
+                return CrewUI().showCrew()
 
             elif selection =='2':
                 #Setja inn villuboð - ath má vera svona mikið í try?
@@ -52,13 +51,13 @@ class DisplayMenuEmployee:
             
 
             elif selection == '3':
-                DisplayMenuPilots(self.logic_layer).startDisplayPilots()
+                return DisplayMenuPilots(self.logic_layer).startDisplayPilots()
 
             elif selection == '4':
-                DisplayMenuAttendants(self.logic_layer).startDisplayAttendants()
+                return DisplayMenuAttendants(self.logic_layer).startDisplayAttendants()
             
             elif selection == '5':
-                DisplayMenuWorkingCrew().startDisplayMenuWorkingCrew()
+                return DisplayMenuWorkingCrew().startDisplayMenuWorkingCrew()
             
             elif selection == '6':
                 print('Enter the "From date" for work schedule')
@@ -71,10 +70,7 @@ class DisplayMenuEmployee:
                 end_month_int = int(input('Month: '))
                 end_day_int = int(input('Day: '))
                 end_date = datetime.datetime(end_year_int,end_month_int,end_day_int,0,0,0).isoformat()
-                
-            
-            elif selection == 'm':
-                return
+
 
             else: 
                 print('Invalid selection')
