9c87c9f3e9bcf6b10e907dc860265ad0e54fda0c
kingamaris <54936816+kingamaris@users.noreply.github.com>
2019-12-05 10:31:24 +0000

    destination format done

--- NaNair/ModelClasses/destination_model.py
+++ NaNair/ModelClasses/destination_model.py
@@ -1,13 +1,15 @@
 class Destination:
-    def __init__(self,name,airport,distance,contact,emergency_phone_number,duration):
+    def __init__(self,name,airport,distance,duration,contact,emergency_phone_number,):
         self.__name = name
         self.__airport = airport
         self.__distance = distance
         self.__contact = contact
         self.__emergency_phone_number = emergency_phone_number
         self.__duration = duration
-
+#id,destination,flight duration,distance,emergency name,emergency phone
     def __str__(self):
+        format_str = '{:<15}{:<15}{:<10}{:<12}{:<15}{:>12}'.format(self.__name,self.__airport,self.__distance,self.__duration,self.__contact,self.__emergency_phone_number)
         
-        return str(self.__name) +','+str(self.__airport) + ','+str(self.__distance)+','+str(self.__contact)+','+str(self.__emergency_phone_number)+','+str(self.__duration)
+        return format_str
+
 
