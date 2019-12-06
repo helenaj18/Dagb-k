e7e64d549f2f30f3d2f41dc76a5811f25fc77649
Marinella <marinella16@ru.is>
2019-12-06 12:37:01 +0000

    flight att model

--- NaNair/ModelClasses/flight_att_model.py
+++ NaNair/ModelClasses/flight_att_model.py
@@ -1,7 +1,7 @@
 from ModelClasses.crew_model import Crew
 
 class FlightAttendant(Crew):
-    def __init__(self, name, crewID, address='', phonenumber='', email='', head_flight_att=False, role = ''):
+    def __init__(self, name, crewID, address='', phonenumber='', email='', head_flight_att=False, license = '',role = ''):
         Crew.__init__(self, name, crewID, address, phonenumber, email)
         self.__head_flight_att = bool(head_flight_att)
         self.__license = 'N/A'
