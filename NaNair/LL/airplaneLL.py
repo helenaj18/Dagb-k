305ebc824c3584c188a03a2fc464a3c0ef2ea9e4
Helena <helenaj18@ru.is>
2019-12-06 14:35:57 +0000

    pycahe

--- NaNair/LL/airplaneLL.py
+++ NaNair/LL/airplaneLL.py
@@ -197,30 +197,30 @@ class AirplaneLL:
         available_airplanes_list = []
 
         if airplanes_on_date != None:
-
+            # Gets the hour out of the datetime string
             hour_int = int(datetime_str[11:13])
 
             for item in airplanes_on_date:
                 airplane = item[0]
                 destination = item[1]
-                departure_time = item[2]
-                arrival_time_out = item[3]
-                arrival_time_home = item[4]
+                departure_time_datetime_str = item[2]
+                arrival_time_out_datetime_str  = item[3]
+                arrival_time_home_datetime_str  = item[4]
                 flight_number_out,flight_number_home = item[5]
 
-                departure_hour_int = int(departure_time[11:13])
-                arrival_hour_out_int = int(arrival_time_out[11:13])
-                arrival_hour_home_int = int(arrival_time_home[11:13])
+                departure_hour_int = int(departure_time_datetime_str [11:13])
+                arrival_hour_out_int = int(arrival_time_out_datetime_str [11:13])
+                arrival_hour_home_int = int(arrival_time_home_datetime_str [11:13])
 
                 if departure_hour_int<=hour_int<=arrival_hour_home_int:
                     # If the hours is between departure hour and arrival at destination,
                     # the flight number out is added
                     if departure_hour_int<=hour_int<=arrival_hour_out_int:
-                        not_available_airplanes_list.append((airplane,destination,arrival_time_home,flight_number_out))
+                        not_available_airplanes_list.append((airplane,destination,arrival_time_home_datetime_str,flight_number_out))
                     
                     # Else the flight number home is added
                     else:
-                        not_available_airplanes_list.append((airplane,destination,arrival_time_home,flight_number_home))
+                        not_available_airplanes_list.append((airplane,destination,arrival_time_home_datetime_str,flight_number_home))
                 else:
                     available_airplanes_list.append(airplane)
 
