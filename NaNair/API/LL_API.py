63a2fa4e569ee6cd9dc50bc788398add959c946a
kingamaris <54936816+kingamaris@users.noreply.github.com>
2019-12-06 10:50:02 +0000

    format

--- NaNair/API/LL_API.py
+++ NaNair/API/LL_API.py
@@ -131,11 +131,15 @@ class LL_API:
     def changeDestinationEmergencyContact(self):
         destination_name = input('Enter airport code (IATA): ')
         new_emergency_contact = input('Enter new emergency contact: ')
+        print('\nNew Emergency Contact ({}) for {} has been saved.\n'.format(new_emergency_contact,destination_name))
+        
         return DestinationLL().changeEmergencyContactName(destination_name,new_emergency_contact)
 
     def changeDestinationEmergencyPhone(self):
         destination_name = input('Enter airport code (IATA): ')
-        new_emergency_phone = input('Enter new emergency phone number: ')        
+        new_emergency_phone = input('Enter new emergency phone number: ') 
+        print('\nNew Emergency Phone Number ({}) for {} has been saved.\n'.format(new_emergency_phone,destination_name))
+               
         return DestinationLL().changeEmergencyContactPhone(destination_name,new_emergency_phone)
 
 #LL_API.show_all_planes()
