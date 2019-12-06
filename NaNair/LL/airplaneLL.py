d130da2d3c8bb9fde26077c62580258796877ddc
Helena <helenaj18@ru.is>
2019-12-06 10:38:49 +0000

    bæta við airplanes by datetime, virkni eftir

--- NaNair/LL/airplaneLL.py
+++ NaNair/LL/airplaneLL.py
@@ -1,14 +1,35 @@
 from API.IO_API import IO_API
 from IO.airplaneIO import AirplaneIO
 from ModelClasses.airplane_model import Airplane
+from LL.voyageLL import VoyageLL
 
 class AirplaneLL:
     ''' LL class for airplane '''
 
     def getAirplanes(self):
-        '''Fetches list of airplanes and returns a list'''
+        '''Returns a list of airplane instances'''
         return AirplaneIO().loadAirplaneFromFile()
+    
+    def getAirplanesByDate(self,date_str):
+        airplane_list = AirplaneIO().loadAirplaneFromFile()
+        voyages_on_date = VoyageLL().getVoyageInDateRange(date_str,date_str)
+        airplanes_on_date = []
+
+        for voyage in voyages_on_date:
+            duration = voyage.getDestination().getDestinationDuration()
+            destination = voyage.getDestinationName()
+            for airplane in airplane_list:
+                if voyage.getAircraftID() == airplane.get_planeInsignia():
+                    airplanes_on_date.append((airplane,duration,destination))
         
+        return airplanes_on_date
+    
+    def getAirplanesByDateTime(self,date_str):
+        airplanes_on_date = self.getAirplanesByDate(date_str)
+
+        pass
+
+
     def getAirplanesByType(self, planeTypeID = ''):
         ''' Returns list of airplanes with same Id'''
         airplanes_type_list = []
--- NaNair/UI/airplaneUI.py
+++ NaNair/UI/airplaneUI.py
@@ -17,7 +17,83 @@ class AirplaneUI:
             print(elem)
         print()
 
-        
+    def checkIfDateValid(self,year_int,month_int,day_int):
+        '''Checks if date is valid'''
+        # Ath á kannski að vera eh year today? spyrja völu hún gerði þetta eh staðar
+        if 0<year_int<=2020:
+            if 0<month_int<=12:
+                months_with_30_days = [1,3,5,7,8,10,12]
+                if month_int in months_with_30_days:
+                    if 0<day_int<=31:
+                        return year_int,month_int,day_int
+                    else:
+                        return None
+                elif month_int == 2:
+                    if 0<day_int<=28:
+                        return year_int,month_int,day_int
+                    else:
+                        return None
+                else:
+                    if 0<day_int<=30:
+                        return year_int,month_int,day_int
+                    else:
+                        return None
+            else:
+                return None
+        else:
+            return None
+
+    def VerifyDate(self,year_str,month_str,day_str,hour_str,minute_str):
+        while True:
+            try:
+                year_int = int(year_str)
+                month_int = int(month_str)
+                day_int = int(day_str)
+                
+                date_tuple = self.checkIfDateValid(year_int,month_int,day_int)
+                if date_tuple != None:
+                    return date_tuple
+                else:
+                    print('Invalid date! Try again: ')
+                    year_str = input('Year: ')
+                    month_str = input('Month: ')
+                    day_str = input('Day: ')
+
+            except ValueError:
+                print('Invalid date! Try again: ')
+                year_str = input('Year: ')
+                month_str = input('Month: ')
+                day_str = input('Day: ')
+    
+    def VerifyTime(self,hour_str,minute_str):
+        while True:
+            try:
+                hour_int = int(hour_str)
+                minute_int = int(minute_str)
+
+                hour_minute_tuple = self.checkIfTimeValid(hour_int,minute_int)
+                if hour_minute_tuple != None:
+                    
+                    return hour_minute_tuple
+                else:
+                    print('Invalid time! Try again: ')
+                    hour_int = input('Hour: ')
+                    minute_int = input('Minute: ')
+
+            except ValueError:
+                print('Invalid time! Try again: ')
+                hour_str = input('Hour: ')
+                minute_str = input('Minute: ')
+
+    def checkIfTimeValid(self,hour_int,minute_int):
+        if 0<=hour_int<23:
+            if 0<=minute_int<60:
+                return hour_int,minute_int
+            else:
+                return None
+        else:
+            return None
+    
     def showAirplanesByType(self,planeTypeID):
         '''Shows Airplanes by type'''
 
@@ -28,19 +104,14 @@ class AirplaneUI:
         print('-'*len(header_str))
 
         for airplane in airplanes:
-            planeInsignia = airplane.get_planeInsignia()
-            planeTypeID = airplane.get_planeTypeID()
-            manufacturer = airplane.get_planeManufacturer()
-            model = airplane.get_planeModel()
-            capacity = airplane.get_planeCapacity()
-            length = airplane.get_planeCapacity()
-            height = airplane.get_planeHeight()
-            wingspan = airplane.get_planeWingspan()
             
-            format_str = '{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}'.format(planeInsignia,planeTypeID,manufacturer,model,capacity,length,height,wingspan)       
+            format_str = '{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}'.format(airplane.get_planeInsignia(),airplane.get_planeTypeID(),airplane.get_planeManufacturer(),airplane.get_planeModel(),airplane.get_planeCapacity(),airplane.get_planeLength(),airplane.get_planeHeight(),airplane.get_planeWingspan())       
             print(format_str)
             
         print()
+    
+    def showAirplanesByDateTime(self,date_str):
+        return LL_API().showAirplanesByDateTime()
 
         
     def addAirplane(self):
